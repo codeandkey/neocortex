efeae4123ab3a3d9d61cb6dbef8ce9920f42b242
codeandkey <jtst@iastate.edu>
2020-01-16 16:25:49 -0600

    add basic lookup test to main

--- src/neocortex.cpp
+++ src/neocortex.cpp
@@ -1,8 +1,22 @@
 #include "log.h"
+#include "lookup.h"
+#include "bitboard.h"
+
+#include <iostream>
 
 int main(int argc, char** argv) {
     nc::log_init();
     nc_info("Started neocortex.");
+
+    nc::lookup::init();
+    nc_info("Built attack lookups.");
+
+    nc::Occtable occ = nc::Occtable::standard();
+
+    u64 b;
+
+    b = nc::lookup::queen_attack(std::string("h2"), &occ);
+    std::cout << nc::bitboard::to_string(b);
 
     return 0;
 }
