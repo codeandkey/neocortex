79586783305cf8d9f7c37393515859c8d67b4d91
codeandkey <jtst@iastate.edu>
2020-02-10 15:16:23 -0600

    unit_test: cleanup

--- src/unit_test.h
+++ src/unit_test.h
@@ -9,10 +9,24 @@
 namespace nc2 {
     class UnitTest {
         public:
+            /**
+             * Initialize a unit test.
+             *
+             * @param test_name Name for test.
+             */
             UnitTest(std::string test_name);
 
+            /**
+             * Runs the unit test.
+             * Should raise an exception if the test fails.
+             */
             virtual void run() = 0;
 
+            /**
+             * Gets the name of this test.
+             *
+             * @return Test name
+             */
             std::string get_name();
 
         private:
