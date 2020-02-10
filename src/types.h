adf2b0c0dffb627b70597359fc661077d52115d6
codeandkey <jtst@iastate.edu>
2020-02-10 15:15:28 -0600

    types: cleanup

--- src/types.h
+++ src/types.h
@@ -9,5 +9,12 @@ namespace nc2 {
     typedef uint32_t u32;
     typedef uint64_t u64;
 
+    /**
+     * Gets a printable form of a bitboard.
+     * Contains newlines after every rank.
+     *
+     * @param bb Input bitboard.
+     * @return Printable bitboard string.
+     */
     std::string bitboard_to_string(u64 bb);
 }
