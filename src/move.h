31b2bef8a60eacd0ea510224a7f9772c126c9593
codeandkey <jtst@iastate.edu>
2020-02-10 16:06:59 -0600

    move: add docs

--- src/move.h
+++ src/move.h
@@ -13,13 +13,46 @@
 namespace nc2 {
     class Move {
         public:
+            /**
+             * Initializes a null move.
+             */
             Move();
+
+            /**
+             * Initializes a uci move.
+             *
+             * @param from From square.
+             * @param to To square.
+             * @param ptype Promotion type.
+             */
             Move(u8 from, u8 to, u8 ptype = piece::Type::NONE);
 
+            /**
+             * Gets the source square.
+             *
+             * @return From square.
+             */
             u8 get_from();
+
+            /**
+             * Gets the destination square.
+             *
+             * @return To square.
+             */
             u8 get_to();
+
+            /**
+             * Gets the promotion type.
+             *
+             * @return Promotion piece type.
+             */
             u8 get_ptype();
 
+            /**
+             * Gets the move as a printable string (UCI notation)
+             *
+             * @return Printable move string.
+             */
             std::string to_string();
 
         private:
