b5d63ab80df2a5d8580fa0632307be02ff28367b
codeandkey <jtst@iastate.edu>
2020-03-05 09:59:27 -0600

    uci: add other half of commit

--- src/uci.h
+++ src/uci.h
@@ -15,13 +15,14 @@
 
 /* Interesting time control: accept an early iteration if it scores better than (remaining_ms / score_fraction) */
 #define NC_UCI_ACCEPTABLE_SCORE_FRACTION 4
+#define NC_UCI_ACCEPTABLE_SCORE_FRACTION_MIN_DEPTH 4
 
 /* 
  * if the evaluation decreases by this amount and the expected search time is <= BLUNDER_REQTIME percent remaining,
  * continue searching
  */
-#define NC_UCI_BLUNDER 80
-#define NC_UCI_BLUNDER_REQTIME 80
+#define NC_UCI_BLUNDER 0
+#define NC_UCI_BLUNDER_REQTIME 100
 
 /* Don't start an iteration if it is expected to consume more than n percent of the remaining time. */
 #define NC_UCI_TIME_FACTOR 70
