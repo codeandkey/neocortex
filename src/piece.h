5ef45b578cf414e47518d30c7d1a39d1a74f61e2
codeandkey <jtst@iastate.edu>
2020-08-06 20:34:44 -0500

    piece: add header docs

--- src/piece.h
+++ src/piece.h
@@ -23,20 +23,94 @@ namespace neocortex {
 
 		constexpr int null = -1;
 
+		/**
+		 * Constructs a piece from a color and type.
+		 *
+		 * @param color Input color.
+		 * @param type Input type.
+		 *
+		 * @return Piece value.
+		 */
 		int make_piece(int color, int type);
+
+		/**
+		 * Gets the color of a piece.
+		 *
+		 * @param piece Input piece.
+		 * @return Piece color.
+		 */
 		int color(int piece);
+
+		/**
+		 * Gets the type of a piece.
+		 *
+		 * @param piece Input piece.
+		 * @return Piece type.
+		 */
 		int type(int piece);
 
+		/**
+		 * Tests if a piece is not null.
+		 *
+		 * @param piece Input piece.
+		 * @return true iff piece is a valid piece.
+		 */
 		bool is_valid(int piece);
+
+		/**
+		 * Tests if a value represents a type.
+		 *
+		 * @param piece Input type.
+		 * @return true iff piece is a valid type.
+		 */
 		bool is_type(int type);
 
+		/**
+		 * Gets a UCI/FEN character for a piece.
+		 * 
+		 * @param piece Input piece.
+		 * @return Piece character in FEN format.
+		 */
 		char get_uci(int piece);
+
+		/**
+		 * Parses a UCI/FEN character into a piece.
+		 *
+		 * @param uci Input character.
+		 * @return Encoded piece.
+		 */
 		int from_uci(char uci);
 
-		int color_from_uci(char uci); /* parses 'w' or 'b' */
+		/**
+		 * Parses 'w' or 'b' into WHITE and BLACK respectively.
+		 *
+		 * @param uci Input character.
+		 * @return Parsed color.
+		 */
+		int color_from_uci(char uci);
+
+		/**
+		 * Converts a color into 'w' or 'b'.
+		 *
+		 * @param col Input color.
+		 * @return FEN color character ('w' or 'b').
+		 */
 		char color_to_uci(int col);
 
+		/**
+		 * Converts a FEN/UCI piece type char into a type.
+		 *
+		 * @param uci Input character.
+		 * @return Parsed type.
+		 */
 		int type_from_uci(char uci);
+
+		/**
+		 * Converts a piece type into a FEN/UCI piece type char.
+		 *
+		 * @param type Input type.
+		 * @return FEN type character.
+		 */
 		char type_to_uci(int type);
 	}
 }
