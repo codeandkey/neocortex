f78e428115a0b0c77e1344950d74900f48a8eacc
codeandkey <jtst@iastate.edu>
2020-08-06 20:30:05 -0500

    perft: add header docs

--- src/perft.h
+++ src/perft.h
@@ -20,11 +20,38 @@ namespace neocortex {
 
 			double totaltime;
 
+			/**
+			 * Converts the result to a table row for the output.
+			 *
+			 * @param for_depth Depth this result was executed for.
+			 * @return Table row string.
+			 */
 			std::string to_row(int for_depth);
+
+			/**
+			 * Gets a header for the perft result table.
+			 *
+			 * @return Table header string.
+			 */
 			static std::string header();
 		};
 
+		/**
+		 * Runs perft at a single depth.
+		 *
+		 * @param p Root position.
+		 * @param depth Depth to search.
+		 */
 		results run(Position& p, int depth);
+
+		/**
+		 * Runs perft from depth 1 up to the input depth, and outputs
+		 * the result in a table form to a stream.
+		 *
+		 * @param p Root position.
+		 * @param depth Maximum depth to run perft.
+		 * @param out Output stream.
+		 */
 		void start(Position& p, int depth, std::ostream& out);
 	}
 }
